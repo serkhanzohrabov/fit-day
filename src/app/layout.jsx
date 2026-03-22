51dbc423dee5a21a2f56ca579b62d6761aab3385
Serkhan <106021920+southsidescript@users.noreply.github.com>
2026-03-22 15:08:54 +0300

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+   
+        <Script src="https://telegram.org/js/telegram-web-app.js" /> 
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
+import styles from "./page.module.css";
 import Avatar from "@/components/avatar/avatar";
 import TextType from "@/components/textType/textType";
 import Form from "@/components/form/form";
+import { useState , useEffect } from "react";
 
 export default function Home() {
   const name = 'Tim';
+  const [user,setUser] = useState(null)
+
+
    const advices = [
      'Больше овощей и фруктов каждый день.',
      'В каждый приём пищи добавляй белок.' ,
      'Пей достаточно воды, минимум сладких напитков.'
   ];
 
+  useEffect( () => {
+    const tg  = window.Telegram.WebApp;
+    const userData = tg.initDataUnsafe.user;
+
+    tg.expand();
+
+    if(userData){
+      setUser(userData)
+    }
+
+  },[])
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Avatar />
-        <h1>Hello, {name}!</h1>
+        <h1>Hello, {user !== null ? user.first_name : name}!</h1>
         <p> <TextType text={advices}/> </p>
 
         <div className={styles.form}>
